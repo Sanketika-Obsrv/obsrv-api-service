f1ba963d0b4c7ae30346836382dd0d0057740497
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-01-20 14:05:42 +0530

    #1 fix: Bug Fix

--- src/test/fixtures.ts
+++
@@ -1,8 +0,0 @@
-class testIngestionSpec {
-    public static SAMPLE_1 = "";
-    public static SAMPLE_2 = "";
-    public static SAMPLE_3 = "";
-    public static SAMPLE_4 = "";
-    public static SAMPLE_5 = "";
-}
-export { testIngestionSpec }
