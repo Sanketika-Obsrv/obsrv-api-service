b43edce205373cda9e12763ff40fd8be4d3d4606
Jerald <jeraldj@sanketika.in>
2024-03-11 18:34:12 +0530

    Sanketika-Obsrv/issue-tracker#77: feat: configuring winston logger

--- api-service/src/app.ts
+++ api-service/src/app.ts
@@ -1,13 +1,13 @@
 import express, { Application } from "express";
 import { config } from "./configs/Config";
-// import { ResponseHandler } from "./helpers/ResponseHandler";
-// import { loadExtensions } from "./managers/Extensions";
-// import { router } from "./routes/Router";
+import { ResponseHandler } from "./helpers/ResponseHandler";
+import { router } from "./routes/Router";
 import bodyParser from "body-parser";
-// import { interceptAuditEvents } from "./services/telemetry";
+import { interceptAuditEvents } from "./services/telemetry";
 import { queryService } from "./routes/Router";
 import { routesConfig } from "./configs/RoutesConfig";
 import { QueryValidator } from "./validators/QueryValidator";
+import logger from "./logger";
 const app: Application = express();
 const queryValidator = new QueryValidator();
 
@@ -17,23 +17,19 @@ const services = {
   nativeQueryId: routesConfig.query.native_query.api_id,
   sqlQueryId: routesConfig.query.sql_query.api_id,
 }
- 
-app.use(bodyParser.json({ limit: config.body_parser_limit}));
+
+app.use(bodyParser.json({ limit: config.body_parser_limit }));
 app.use(express.text());
 app.use(express.json());
 app.set("queryServices", services);
+app.use(interceptAuditEvents())
+app.use("/", router);
+app.use("*", ResponseHandler.routeNotFound);
+app.use(ResponseHandler.errorResponse);
 
-// loadExtensions(app)
-//   .finally(() => {
-//     app.use(interceptAuditEvents())
-//     app.use("/", router);
-//     app.use("*", ResponseHandler.routeNotFound);
-//     app.use(ResponseHandler.errorResponse);
-
-//     app.listen(config.api_port, () => {
-//       console.log(`listening on port ${config.api_port}`);
-//   });
-// });
+app.listen(config.api_port, () => {
+  logger.info(`listening on port ${config.api_port}`);
+});
 
 
 export default app;
