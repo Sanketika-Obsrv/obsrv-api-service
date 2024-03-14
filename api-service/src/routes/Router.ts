07021bbfaae3887691ea12c5e15e7e6c035ff06e
yashashkumar <yashashk@sanketika.in>
2024-03-14 15:22:58 +0530

    Sanketika-obsrv/issue-tracker#70: Removed unused imports

--- api-service/src/routes/Router.ts
+++ api-service/src/routes/Router.ts
@@ -2,7 +2,6 @@ import express from "express";
 import dataIn from "../controllers/DataIngestion/DataIngestionController";
 
 export const router = express.Router();
-export const queryService=""
 
 /** Ingestor API */
 router.post(`/v1/data/in/:datasetId`, dataIn);
