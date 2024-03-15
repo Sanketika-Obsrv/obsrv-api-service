c306b9a3b09e9e620d5383527acc7fe7c2aaf673
yashashkumar <yashashk@sanketika.in>
2024-03-15 11:05:42 +0530

    Sanketika-obsrv/issue-tracker#70: removed comment

--- api-service/src/routes/Router.ts
+++ api-service/src/routes/Router.ts
@@ -3,5 +3,4 @@ import dataIn from "../controllers/DataIngestion/DataIngestionController";
 
 export const router = express.Router();
 
-/** Ingestor API */
 router.post(`/v1/data/in/:datasetId`, dataIn);
