42754fc6d9c0fb58dfc27edce8fc7dd9db2f0cf6
yashashkumar <yashashk@sanketika.in>
2024-03-15 16:26:53 +0530

    Sanketika-obsrv/issue-tracker#70: added metadata while sending events

--- api-service/src/connections/kafkaConnection.ts
+++ api-service/src/connections/kafkaConnection.ts
@@ -1,6 +1,9 @@
 import { Kafka } from "kafkajs";
 import { connectionConfig } from '../configs/ConnectionsConfig'
 import logger from "../logger";
+import * as _ from "lodash";
+import { config } from "../configs/Config";
+import { v4 } from "uuid";
 
 const kafka = new Kafka(connectionConfig.kafka.config);
 const producer = kafka.producer();
@@ -22,6 +25,14 @@ export const send = async (payload: Record<string, any>, topic: string) => {
   if (!isConnected) {
     await connect()
   }
+
+  const now = Date.now();
+  _.set(payload, 'syncts', now);
+  if (!payload?.mid) _.set(payload, 'mid', v4());
+  const source = { meta: { id: "", connector_type: "api", version: config?.version, entry_source: "api" }, trace_id: v4() };
+  const obsrvMeta = { syncts: now, processingStartTime: now, flags: {}, timespans: {}, error: {}, source: source };
+  _.set(payload, 'obsrv_meta', obsrvMeta);
+
   return producer.send({
     topic: topic,
     messages: [{ value: JSON.stringify(payload) }]
