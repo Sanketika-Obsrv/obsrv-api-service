2c83fe47fa67a349798bf044a60891d8eed9520f
yashash <126703764+yashashkumar@users.noreply.github.com>
2024-03-15 18:04:12 +0530

    Data in api refactor (#104)
    * Sanketika-obsrv/issue-tracker#70: added sudocode function templates for data ingest api.
    
    * Sanketike-obsrv/issue-tracker#70: changed api id
    
    * Sanketika-obsrv/issue-tracker#70: added modle for dataset live table
    
    * Sanketika-obsrv/issue-tracker#70: minor changes in the schema
    
    * Sanketika-obsrv/issue-tracker#70: added dataingestion controller method and its implementation
    
    * Sanketika-obsrv/issue-tracker#70: removed unused imports
    
    * Sanketika-obsrv/issue-tracker#70: removed unused imports
    
    * Sanketika-obsrv/issue-tracker#70: handling kafka connection using boolean
    
    * Sanketika-obsrv/issue-tracker#70: changes in schema
    
    * Sanketika-obsrv/issue-tracker#70: Added dataset service
    
    * Sanketika-obsrv/issue-tracker#70: Methods refactoring
    
    * Sanketika-obsrv/issue-tracker#70: Added schema validation seperately
    
    * Sanketika-obsrv/issue-tracker#70: Removed unused imports
    
    * Sanketika-obsrv/issue-tracker#70: handling errors
    
    * Sanketika-obsrv/issue-tracker#70: removed comment
    
    * Sanketika-obsrv/issue-tracker#70: added metadata while sending events
    
    * Sanketika-obsrv/issue-tracker#70: review comments issue resolved
    
    * Sanketika-obsrv/issue-tracker#70: review comments issue resolved
    
    * Sanketika-obsrv/issue-tracker#70: removed unused import
    
    * Sanketika-obsrv/issue-tracker#70: added function to update metadata in controller.
    
    * Sanketika-obsrv/issue-tracker#70: removed unused imports
    
    * Sanketika-obsrv/issue-tracker#70: added logger message to error
    
    * Sanketika-obsrv/issue-tracker#70: added datasetId in the metadata
    
    * Sanketika-obsrv/issue-tracker#70: refactored nested conditions
    
    * Sanketika-obsrv/issue-tracker#70: removed logger

--- api-service/src/connections/kafkaConnection.ts
+++ api-service/src/connections/kafkaConnection.ts
@@ -1,20 +1,29 @@
 import { Kafka } from "kafkajs";
 import { connectionConfig } from '../configs/ConnectionsConfig'
+import logger from "../logger";
 
 const kafka = new Kafka(connectionConfig.kafka.config);
 const producer = kafka.producer();
 
+let isConnected = false;
 
-export const connect = async() => {
-    await producer.connect()
-      .then(() => {
-        console.log("kafka dispatcher is ready");
-      })
-      .catch((err) => {
-        console.error("Unable to connect to kafka", err.message);
-      });
+export const connect = async () => {
+  await producer.connect()
+    .then(() => {
+      isConnected = true;
+      logger.info("kafka dispatcher is ready");
+    })
+    .catch((err) => {
+      logger.error("Unable to connect to kafka", err?.message);
+    });
+}
+
+export const send = async (payload: Record<string, any>, topic: string) => {
+  if (!isConnected) {
+    await connect()
   }
-
-export const send = async () => {
-   
+  return producer.send({
+    topic: topic,
+    messages: [{ value: JSON.stringify(payload) }]
+  });
 }
---
+++ api-service/src/controllers/DataIngestion/DataIngestionController.ts
@@ -0,0 +1,82 @@
+import { Request, Response } from "express";
+import * as _ from 'lodash';
+import validationSchema from "./validationSchema.json";
+import { schemaValidation } from "../../services/ValidationService";
+import { ResponseHandler } from "../../helpers/ResponseHandler";
+import { send } from "../../connections/kafkaConnection";
+import { getDataset } from "../../services/DatasetService";
+import logger from "../../logger";
+import { v4 } from "uuid";
+import { config } from "../../configs/Config";
+
+const errorObject = {
+    invalidRequestBody: {
+        "message": "Invalid dataset config provided for ingestion",
+        "statusCode": 400,
+        "errCode": "BAD_REQUEST"
+    },
+    datasetNotFound: {
+        "message": "Dataset with id not found",
+        "statusCode": 404,
+        "errCode": "BAD_REQUEST"
+    },
+    topicNotFound: {
+        "message": "Entry topic is not defined",
+        "statusCode": 404,
+        "errCode": "BAD_REQUEST"
+    }
+}
+
+const dataIn = async (req: Request, res: Response) => {
+    try {
+        const datasetId = req.params.datasetId.trim();
+        const isValidSchema = schemaValidation(req.body, validationSchema)
+        if (!isValidSchema?.isValid) {
+            return ResponseHandler.errorResponse({ message: isValidSchema?.message, statusCode: 400, errCode: "BAD_REQUEST" }, req, res);
+        }
+        const dataset = await getDataset(datasetId)
+        if (!dataset) {
+            logger.error(`Dataset with id ${datasetId} not found in live table`)
+            return ResponseHandler.errorResponse(errorObject.datasetNotFound, req, res);
+        }
+        const validData = validation(req.body?.data, dataset);
+        const entryTopic = _.get(dataset, "dataValues.dataset_config.entry_topic")
+        if (!entryTopic) {
+            logger.error("Entry topic not found")
+            return ResponseHandler.errorResponse(errorObject.topicNotFound, req, res);
+        }
+        await send(addMetadataToEvents(validData, datasetId), _.get(dataset, "dataValues.dataset_config.entry_topic"))
+        ResponseHandler.successResponse(req, res, { status: 200, data: { message: "Data ingested successfully" } });
+    }
+    catch (err: any) {
+        logger.error(err)
+        ResponseHandler.errorResponse(err, req, res);
+    }
+}
+
+export const validation = (data: any, dataset: Record<string, any>) => {
+    const extractionKey = _.get(dataset?.dataValues, "extraction_config.extraction_key");
+    const isBatchEvent = _.get(dataset?.dataValues, "extraction_config.is_batch_event");
+    const batchIdentifier = _.get(dataset?.dataValues, "extraction_config.batch_id")
+
+    if (!_.isArray(_.get(data, "event"))) {
+        return data;
+    }
+    if (isBatchEvent && (_.has(data, extractionKey) && _.has(data, batchIdentifier))) {
+        return data;
+    }
+    throw errorObject.invalidRequestBody;
+}
+
+const addMetadataToEvents = (payload: any, datasetId: string) => {
+    const now = Date.now();
+    _.set(payload, 'syncts', now);
+    _.set(payload, 'dataset', datasetId);
+    if (!payload?.mid) _.set(payload, 'mid', v4());
+    const source = { meta: { id: "", connector_type: "api", version: config?.version, entry_source: "api" }, trace_id: v4() };
+    const obsrvMeta = { syncts: now, processingStartTime: now, flags: {}, timespans: {}, error: {}, source: source };
+    _.set(payload, 'obsrv_meta', obsrvMeta);
+    return payload;
+}
+
+export default dataIn;
