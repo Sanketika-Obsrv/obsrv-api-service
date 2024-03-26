558b0f67ed4e8a62f51b3bd19a72db8caecdc9ce
yashashkumar <yashashk@sanketika.in>
2024-03-26 10:49:23 +0530

    Sanketika-obsrv/issue-tracker#96: added route.

--- api-service/src/routes/Router.ts
+++ api-service/src/routes/Router.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import dataIn from "../controllers/DataIngestion/DataIngestionController";
 import DatasetCreate from "../controllers/DatasetCreate/DatasetCreate";
+import dataOut from "../controllers/DataOut/DataOutController";
 
 export const router = express.Router();
 
 router.post(`/v1/data/in/:datasetId`, dataIn);
+
+router.post('/v1/data/query', dataOut);
 
 router.post("/v1/datasets/create", DatasetCreate)
