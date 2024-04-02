750b6dc55d430c535f0daa78ead5879926ae74ee
Jerald <jeraldj@sanketika.in>
2024-04-02 14:44:44 +0530

    Sanketika-Obsrv/issue-tracker#69: fix: sceanrios and loggers added

--- api-service/src/controllers/DatasetRead/DatasetRead.ts
+++ api-service/src/controllers/DatasetRead/DatasetRead.ts
@@ -1,13 +1,61 @@
 import { Request, Response } from "express";
 import { DatasetStatus } from "../../types/DatasetModels";
+import _, { difference } from "lodash";
+import { validDatasetFields, validDraftDatasetFields } from "../../configs/DatasetConfigDefault";
+import { ResponseHandler } from "../../helpers/ResponseHandler";
+import { ErrorObject } from "../../types/ResponseModel";
+import { query } from "../../connections/databaseConnection";
+import logger from "../../logger";
+import httpStatus from "http-status";
 
 const datasetRead = async (req: Request, res: Response) => {
     try {
         const { dataset_id } = req.params;
         const { fields, status = DatasetStatus.Live } = req.query;
-        
-        
-    } catch (error) {
 
+        const invalidFields = getInvalidFields({ datasetFields: fields, status })
+        if (!_.isEmpty(fields) && !_.isEmpty(invalidFields)) {
+            logger.error(`The specified fields [${invalidFields}] in the dataset cannot be found`)
+            return ResponseHandler.errorResponse({
+                message: `The specified field [${invalidFields}] in the dataset cannot be found.`,
+                statusCode: 400,
+                errCode: "BAD_REQUEST"
+            } as ErrorObject, req, res);
+        }
+
+        const datasetModel = getDatasetModel(status);
+        const fieldValue = !_.isEmpty(fields) ? fields : "*"
+        const { results } = await query(`SELECT ${fieldValue} FROM ${datasetModel} WHERE id = '${dataset_id}'`)
+        if (_.isEmpty(results)) {
+            logger.error(`Dataset with the given dataset_id:${dataset_id} not found`)
+            return ResponseHandler.errorResponse({
+                message: "Dataset with the given dataset_id not found",
+                statusCode: 404,
+                errCode: "NOT_FOUND"
+            } as ErrorObject, req, res);
+        }
+
+        logger.info(`Dataset Read Successfully with id:${dataset_id}`)
+        ResponseHandler.successResponse(req, res, { status: httpStatus.OK, data: _.get(results, "0") as any });
+    } catch (error: any) {
+        logger.error(error)
+        ResponseHandler.errorResponse(error, req, res)
     }
 }
+
+const getDatasetModel = (status: string | any): string => {
+    if (status !== DatasetStatus.Live) return "datasets_draft";
+    return "datasets";
+}
+
+const getInvalidFields = (payload: Record<string, any>): Record<string, any> => {
+    const { datasetFields, status } = payload
+    if (status == DatasetStatus.Live) {
+        const invalidFields = _.difference(_.split(datasetFields, ","), validDatasetFields)
+        return invalidFields
+    }
+    const invalidFields = difference(_.split(datasetFields, ","), validDraftDatasetFields)
+    return invalidFields;
+}
+
+export default datasetRead;
--- api-service/src/routes/Router.ts
+++ api-service/src/routes/Router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dataIn from "../controllers/DataIngestion/DataIngestionController";
 import DatasetCreate from "../controllers/DatasetCreate/DatasetCreate";
 import DatasetUpdate from "../controllers/DatasetUpdate/DatasetUpdate";
+import DatasetRead from "../controllers/DatasetRead/DatasetRead";
 import dataOut from "../controllers/DataOut/DataOutController";
 
 
@@ -11,3 +12,4 @@ router.post(`/v1/data/in/:datasetId`, dataIn);
 router.post('/v1/data/query', dataOut);
 router.post("/v1/datasets/create", DatasetCreate)
 router.patch("/v1/datasets/update", DatasetUpdate)
+router.get("/v1/datasets/read/:dataset_id", DatasetRead)
