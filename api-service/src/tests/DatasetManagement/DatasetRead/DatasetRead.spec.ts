3285ceff4863d723aa1e4382d9657d2e60ad2a6b
JeraldJF <jeraldj@sanketika.in>
2024-04-08 10:50:32 +0530

    Sanketika-Obsrv/issue-tracker#69: test case assertion updated in fixtures

--- api-service/src/tests/DatasetManagement/DatasetRead/DatasetRead.spec.ts
+++ api-service/src/tests/DatasetManagement/DatasetRead/DatasetRead.spec.ts
@@ -7,6 +7,7 @@ import { describe, it } from 'mocha';
 import _ from "lodash";
 import { sequelize } from "../../../connections/databaseConnection";
 import { apiId } from "../../../controllers/DatasetRead/DatasetRead";
+import { TestInputsForDatasetRead } from "./Fixtures";
 
 chai.use(spies);
 chai.should();
@@ -40,7 +41,7 @@ describe("DATASET READ API", () => {
 
     it("Dataset read success: Fetch all dataset fields when fields param is empty", (done) => {
         chai.spy.on(sequelize, "query", () => {
-            return Promise.resolve([[{ name: 'sb-telemetry', id: "sb", status: "Draft", type: "dataset" }], {}])
+            return Promise.resolve([[TestInputsForDatasetRead.DRAFT_SCHEMA], {}])
         })
         chai
             .request(app)
@@ -53,13 +54,15 @@ describe("DATASET READ API", () => {
                 res.body.result.should.be.a("object")
                 res.body.result.type.should.be.eq('dataset')
                 res.body.result.status.should.be.eq('Draft')
+                const result = JSON.stringify(res.body.result)
+                result.should.be.eq(JSON.stringify(TestInputsForDatasetRead.DRAFT_SCHEMA))
                 done();
             });
     });
 
-    it("Dataset read success: Fetch all live dataset when status param is empty", (done) => {
+    it("Dataset read success: Fetch live dataset when status param is empty", (done) => {
         chai.spy.on(sequelize, "query", () => {
-            return Promise.resolve([[{ name: 'sb-telemetry', id: "sb", status: "Live", type: "dataset" }], {}])
+            return Promise.resolve([[TestInputsForDatasetRead.LIVE_SCHEMA], {}])
         })
         chai
             .request(app)
@@ -71,24 +74,8 @@ describe("DATASET READ API", () => {
                 res.body.params.status.should.be.eq("SUCCESS")
                 res.body.result.should.be.a("object")
                 res.body.result.status.should.be.eq('Live')
-                done();
-            });
-    });
-
-    it("Dataset read success: Fetch all live dataset when status param is empty", (done) => {
-        chai.spy.on(sequelize, "query", () => {
-            return Promise.resolve([[{ name: 'sb-telemetry', id: "sb", status: "Draft", type: "dataset" }], {}])
-        })
-        chai
-            .request(app)
-            .get("/v1/datasets/read/sb-telemetry?status=Draft")
-            .end((err, res) => {
-                res.should.have.status(httpStatus.OK);
-                res.body.should.be.a("object")
-                res.body.id.should.be.eq(apiId);
-                res.body.params.status.should.be.eq("SUCCESS")
-                res.body.result.should.be.a("object")
-                res.body.result.status.should.be.eq('Draft')
+                const result = JSON.stringify(res.body.result)
+                result.should.be.eq(JSON.stringify(TestInputsForDatasetRead.LIVE_SCHEMA))
                 done();
             });
     });
