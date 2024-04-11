4e1dbc2bcfc3e63ca64f24306e6b3395df7c4a1d
yashashk <yashashk@sanketika.in>
2024-04-11 15:37:39 +0530

    Sanketika-obsrv/issue-tracker #72 : added required dependencies.

--- api-service/src/routes/Router.ts
+++ api-service/src/routes/Router.ts
@@ -5,7 +5,9 @@ import DatasetUpdate from "../controllers/DatasetUpdate/DatasetUpdate";
 import DatasetRead from "../controllers/DatasetRead/DatasetRead";
 import dataOut from "../controllers/DataOut/DataOutController";
 import { setApiId } from "../middlewares/setApiId";
-
+import { config } from "../configs/Config";
+import { ClientCloudService } from "../controllers/ClientCloudService/ClientCloudService";
+export const exhaustService = new ClientCloudService(config.exhaust_config.cloud_storage_provider, config.exhaust_config.cloud_storage_config);
 
 export const router = express.Router();
 
