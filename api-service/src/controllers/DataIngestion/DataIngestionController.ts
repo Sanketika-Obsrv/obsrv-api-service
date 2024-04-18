bc03413fc28d5ed81f6f4a42af359383a62e7c93
yashashk <yashashk@sanketika.in>
2024-04-18 15:58:20 +0530

    Merge remote-tracking branch 'origin' into data-in

--- api-service/src/controllers/DataIngestion/DataIngestionController.ts
+++ api-service/src/controllers/DataIngestion/DataIngestionController.ts
@@ -6,46 +6,42 @@ import { ResponseHandler } from "../../helpers/ResponseHandler";
 import { send } from "../../connections/kafkaConnection";
 import { getDataset } from "../../services/DatasetService";
 import logger from "../../logger";
-import { v4 } from "uuid";
 import { config } from "../../configs/Config";
 
 const errorObject = {
-    invalidRequestBody: {
-        "message": "Invalid dataset config provided for ingestion",
-        "statusCode": 400,
-        "errCode": "BAD_REQUEST"
-    },
     datasetNotFound: {
         "message": "Dataset with id not found",
         "statusCode": 404,
-        "errCode": "BAD_REQUEST"
+        "errCode": "BAD_REQUEST",
+        "code": "DATASET_NOT_FOUND"
     },
     topicNotFound: {
         "message": "Entry topic is not defined",
         "statusCode": 404,
-        "errCode": "BAD_REQUEST"
+        "errCode": "BAD_REQUEST",
+        "code": "TOPIC_NOT_FOUND"
     }
 }
 
 const dataIn = async (req: Request, res: Response) => {
     try {
+        const requestBody = req.body;
         const datasetId = req.params.datasetId.trim();
-        const isValidSchema = schemaValidation(req.body, validationSchema)
+        const isValidSchema = schemaValidation(requestBody, validationSchema)
         if (!isValidSchema?.isValid) {
-            return ResponseHandler.errorResponse({ message: isValidSchema?.message, statusCode: 400, errCode: "BAD_REQUEST" }, req, res);
+            return ResponseHandler.errorResponse({ message: isValidSchema?.message, statusCode: 400, errCode: "BAD_REQUEST", code: "DATA_INGESTION_INVALID_INPUT" }, req, res);
         }
         const dataset = await getDataset(datasetId)
         if (!dataset) {
             logger.error(`Dataset with id ${datasetId} not found in live table`)
             return ResponseHandler.errorResponse(errorObject.datasetNotFound, req, res);
         }
-        const validData = validation(req.body, dataset);
         const entryTopic = _.get(dataset, "dataValues.dataset_config.entry_topic")
         if (!entryTopic) {
             logger.error("Entry topic not found")
             return ResponseHandler.errorResponse(errorObject.topicNotFound, req, res);
         }
-        await send(addMetadataToEvents(validData, datasetId), _.get(dataset, "dataValues.dataset_config.entry_topic"))
+        await send(addMetadataToEvents(requestBody.data, datasetId, requestBody), _.get(dataset, "dataValues.dataset_config.entry_topic"))
         ResponseHandler.successResponse(req, res, { status: 200, data: { message: "Data ingested successfully" } });
     }
     catch (err: any) {
@@ -54,29 +50,26 @@ const dataIn = async (req: Request, res: Response) => {
     }
 }
 
-export const validation = (requestBody: any, dataset: Record<string, any>) => {
-    const data = requestBody?.data;
-    const extractionKey = _.get(dataset?.dataValues, "extraction_config.extraction_key");
-    const isBatchEvent = _.get(dataset?.dataValues, "extraction_config.is_batch_event");
-    const batchIdentifier = _.get(dataset?.dataValues, "extraction_config.batch_id")
-
-    if (isBatchEvent && (_.has(data, extractionKey) && _.has(data, batchIdentifier))) {
-        return data;
-    }
-    if (!_.isArray(_.get(data, "event"))) {
-        return data;
-    }
-}
-
-const addMetadataToEvents = (payload: any, datasetId: string) => {
+const addMetadataToEvents = (validData: any, datasetId: string, payload: any) => {
     const now = Date.now();
-    _.set(payload, 'syncts', now);
-    _.set(payload, 'dataset', datasetId);
-    if (!payload?.mid) _.set(payload, 'mid', v4());
-    const source = { meta: { id: "", connector_type: "api", version: config?.version, entry_source: "api" }, trace_id: v4() };
-    const obsrvMeta = { syncts: now, processingStartTime: now, flags: {}, timespans: {}, error: {}, source: source };
-    _.set(payload, 'obsrv_meta', obsrvMeta);
-    return payload;
+    const mid = _.get(payload, "params.mid");
+    const source = { id: "api.data.in", version: config?.version, entry_source: "api" };
+    const obsrvMeta = { syncts: now, flags: {}, timespans: {}, error: {}, source: source };
+    if (Array.isArray(validData)) {
+        const payloadRef = validData.map((event: any) => {
+            event = _.set(event, 'obsrv_meta', obsrvMeta);
+            event = _.set(event, 'dataset', datasetId);
+            event = _.set(event, 'mid', mid);
+            return event
+        })
+        return payloadRef;
+    }
+    else {
+        _.set(validData, 'mid', mid);
+        _.set(validData, 'obsrv_meta', obsrvMeta);
+        _.set(validData, 'dataset', datasetId);
+        return validData
+    }
 }
 
 export default dataIn;
