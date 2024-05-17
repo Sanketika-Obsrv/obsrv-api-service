21205be2364b967d702cbecbb47422374f845140
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2024-05-17 15:31:29 +0530

    Sanketika-obsrv/issue-tracker#273 feat: removed unnecessary flatmapping and await

--- api-service/src/helpers/LakehouseUtil.ts
+++ api-service/src/helpers/LakehouseUtil.ts
@@ -1,8 +1,6 @@
 import { Trino, BasicAuth } from 'trino-client';
 import _ from 'lodash';
-import fs from 'fs';
 import { config } from '../configs/Config';
-import constants from '../resources/Constants.json';
 
 const trino: Trino = Trino.create({
     server: `${config.query_api.lakehouse.host}:${config.query_api.lakehouse.port}`,
@@ -11,21 +9,19 @@ const trino: Trino = Trino.create({
     auth: new BasicAuth(config.query_api.lakehouse.default_user),
 });
 
-const JsonFormatting = async (data: any[], columnData: any[]): Promise<any[]> => {
-    
+
+const getFormattedData = (data: any[], columnData: any[]) => {
     const formattedData: any[] = [];
     for (let i = 0; i < data.length; i++) {
         const row = data[ i ];
         const jsonRow: any = {};
         for (let j = 0; j < row.length; j++) {
+            // assign column only if doesn't start with _hoodie_
             const colName = columnData[ j ];
-            jsonRow[ colName ] = row[ j ];
-        }
-        // unset all keys starts with _hoodie_
-        for (const key in jsonRow) {
-            if (key.startsWith("_hoodie_")) {
-                delete jsonRow[ key ];
+            if (_.startsWith(colName, "_hoodie_")) {
+                continue;
             }
+            jsonRow[ colName ] = row[ j ];
         }
         formattedData.push(jsonRow);
     }
@@ -38,24 +34,18 @@ export const executeLakehouseQuery = async (query: string) => {
         let queryResult: any = []
         for await (let data of iter) {
             if(!_.isEmpty(data.error)){
-                switch(data.error.errorType){
-                    case "USER_ERROR":
                         throw {
                             status: 400,
                             message: data.error.message.replace(/line.*: /, ''),
                             code: "BAD_REQUEST" 
                         }
-                    default:
-                        throw constants.FAILED_EXECUTING_QUERY
                 }
-            }
-            queryResult = [ ...queryResult, ...(data.data?.length ? [ data.data ] : []) ]
+            queryResult = [ ...queryResult, ...(data?.data?.length ?  data.data  : []) ]
         }
-        queryResult = _.flatMap(queryResult)
         let columns = await iter.map((r: any) => r.columns ?? []).next();
         let finalColumns = columns.value.map((column: any) => {
             return column.name;
         });
-        const formattedData = await JsonFormatting(queryResult, finalColumns);
+        const formattedData = getFormattedData(queryResult, finalColumns);
         return formattedData
 }
