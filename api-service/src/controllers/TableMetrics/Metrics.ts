a6ef18b9bef768c08ac227b7ba4baa636d609660
yashashk <yashashk@sanketika.in>
2025-01-13 19:17:29 +0530

    feat: #OBS-I321: Added sections in controller

--- api-service/src/controllers/TableMetrics/Metrics.ts
+++ api-service/src/controllers/TableMetrics/Metrics.ts
@@ -2,8 +2,11 @@ import { Request, Response } from "express";
 import * as _ from "lodash"
 import { ResponseHandler } from "../../helpers/ResponseHandler";
 import dayjs from 'dayjs';
-import { handleDataFreshness, handleDataObservability, handleDataVolume } from "./metricsHelper";
+import { handleConnectors, handleDataFreshness, handleDataLineage, handleDataObservability, handleDataQuality, handleDataVolume } from "./metricsHelper";
 import logger from "../../logger";
+import { schemaValidation } from "../../services/ValidationService";
+import validationSchema from "./TableMetrics.json";
+import { config } from "../../configs/Config";
 
 const apiId = "api.table.metrics";
 const tableMetrics = async (req: Request, res: Response) => {
@@ -12,13 +15,17 @@ const tableMetrics = async (req: Request, res: Response) => {
     const requestBody = req.body;
 
     const { category, volume_by_days }: any = req.body.request;
-    const defaultThreshold = 5 * 60 * 1000; // 5 minutes in milliseconds read from env
-    const dateFormat = 'YYYY-MM-DDTHH:mm:ssZ';
+    const defaultThreshold = (typeof config?.default_freshness_threshold === 'number' ? config.default_freshness_threshold : 5) * 60 * 1000; // 5 minutes in milliseconds
+    const dateFormat = 'YYYY-MM-DDTHH:mm:ss';
     const startDate = '2000-01-01T00:00:00+05:30';
     const endDate = dayjs().add(1, 'day').format(dateFormat);
     const intervals = `${startDate}/${endDate}`;
-
+    const isValidSchema = schemaValidation(requestBody, validationSchema);
     const results = [];
+    if (!isValidSchema?.isValid) {
+        logger.error({ apiId, datasetId:dataset_id, msgid, requestBody, message: isValidSchema?.message, code: "DATA_OUT_INVALID_INPUT" })
+        return ResponseHandler.errorResponse({ message: isValidSchema?.message, statusCode: 400, errCode: "BAD_REQUEST", code: "DATA_OUT_INVALID_INPUT" }, req, res);
+    }
 
     try {
         if (!category || category.includes("data_freshness")) {
@@ -35,10 +42,28 @@ const tableMetrics = async (req: Request, res: Response) => {
             const dataVolumeResult = await handleDataVolume(dataset_id, volume_by_days, dateFormat);
             results.push(dataVolumeResult);
         }
+
+        if (!category || category.includes("data_lineage")) {
+            const dataLineageResult = await handleDataLineage(dataset_id, intervals);
+            results.push(dataLineageResult);
+        }
+
+        if (!category || category.includes("connectors")) {
+            const connectorsResult = await handleConnectors(dataset_id, intervals);
+            results.push(connectorsResult);
+        }
+
+        if (!category || category.includes("data_quality")) {
+            const connectorsResult = await handleDataQuality(dataset_id, intervals);
+            results.push(connectorsResult);
+        }
+
         logger.info({ apiId, msgid, requestBody, datasetId: dataset_id, message: "Metrics fetched successfully" })
         return ResponseHandler.successResponse(req, res, { status: 200, data: results });
-    } catch (error: any) {
-        logger.error({ apiId, msgid, requestBody: req?.body, datasetId: dataset_id, message: "Error while fetching metrics", code: "FAILED_TO_FETCH_METRICS" });
+
+    }
+    catch (error: any) {
+        logger.error({ apiId, msgid, requestBody: req?.body, datasetId: dataset_id, message: "Error while fetching metrics", code: "FAILED_TO_FETCH_METRICS", error });
         return ResponseHandler.errorResponse({ message: "Error while fetching metrics", statusCode: 500, errCode: "FAILED", code: "FAILED_TO_FETCH_METRICS" }, req, res);
     }
 
