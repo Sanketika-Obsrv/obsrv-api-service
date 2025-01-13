0098317f89cd98c12aa61007757a6100e4106630
yashashk <yashashk@sanketika.in>
2025-01-13 19:17:10 +0530

    feat: #OBS-I321: Updated queries and request validation schema

--- api-service/src/controllers/TableMetrics/queries.ts
+++ api-service/src/controllers/TableMetrics/queries.ts
@@ -1,126 +1,8 @@
+import dayjs from "dayjs";
+
 export const processingTimeQuery = (intervals: string, dataset: string) => ({
-    query: {
-      queryType: "groupBy",
-      dataSource: "system-events",
-      intervals: intervals,
-      granularity: {
-        type: "all",
-        timeZone: "Asia/Kolkata"
-      },
-      filter: {
-        type: "and",
-        fields: [
-          { type: "selector", dimension: "ctx_module", value: "processing" },
-          { type: "selector", dimension: "ctx_dataset", value: dataset },
-          { type: "selector", dimension: "ctx_pdata_pid", value: "router" },
-          { type: "selector", dimension: "error_code", value: null }
-        ]
-      },
-      aggregations: [
-        { type: "longSum", name: "processing_time", fieldName: "total_processing_time" },
-        { type: "longSum", name: "count", fieldName: "count" }
-      ],
-      postAggregations: [
-        {
-          type: "expression",
-          name: "average_processing_time",
-          expression: "case_searched((count > 0),(processing_time/count),0)"
-        }
-      ]
-    }
-  });
-
-  export const totalEventsQuery = (intervals: string, dataset: string) => ({
-    queryType: "timeseries",
-    dataSource: {
-      type: "table",
-      name: "system-events"
-    },
-    intervals: {
-      type: "intervals",
-      intervals: [intervals]
-    },
-    filter: {
-      type: "equals",
-      column: "ctx_dataset",
-      matchValueType: "STRING",
-      matchValue: dataset
-    },
-    granularity: {
-      type: "all"
-    },
-    aggregations: [
-      {
-        type: "longSum",
-        name: "total_events_count",
-        fieldName: "count"
-      }
-    ],
-    context: {
-      queryId: "02dd33a4-5f3c-4b7e-8d21-56769799d413",
-      sqlOuterLimit: 1001,
-      sqlQueryId: "02dd33a4-5f3c-4b7e-8d21-56769799d413",
-      useNativeQueryExplain: true
-    }
-  });
-  
-  export const totalFailedEventsQuery = (intervals: string, dataset: string) => ({
-    queryType: "timeseries",
-    dataSource: {
-      type: "table",
-      name: "system-events"
-    },
-    intervals: {
-      type: "intervals",
-      intervals: [intervals]
-    },
-    filter: {
-      type: "equals",
-      column: "ctx_dataset",
-      matchValueType: "STRING",
-      matchValue: dataset
-    },
-    granularity: {
-      type: "all"
-    },
-    aggregations: [
-      {
-        type: "filtered",
-        aggregator: {
-          type: "longSum",
-          name: "total_failed_events",
-          fieldName: "count"
-        },
-        filter: {
-          type: "and",
-          fields: [
-            {
-              type: "equals",
-              column: "ctx_pdata_pid",
-              matchValueType: "STRING",
-              matchValue: "validator"
-            },
-            {
-              type: "equals",
-              column: "error_pdata_status",
-              matchValueType: "STRING",
-              matchValue: "failed"
-            }
-          ]
-        },
-        name: "total_failed_events"
-      }
-    ],
-    context: {
-      queryId: "acf3990b-1c9e-42a6-9960-eb85a115ca65",
-      sqlOuterLimit: 1001,
-      sqlQueryId: "acf3990b-1c9e-42a6-9960-eb85a115ca65",
-      useNativeQueryExplain: true
-    }
-  });
-
-  export const generateTimeseriesQuery = (intervals: string, dataset: string) => ({
-    queryType: "timeseries",
+  query: {
+    queryType: "groupBy",
     dataSource: "system-events",
     intervals: intervals,
     granularity: {
@@ -137,6 +19,377 @@ export const processingTimeQuery = (intervals: string, dataset: string) => ({
       ]
     },
     aggregations: [
+      { type: "longSum", name: "processing_time", fieldName: "total_processing_time" },
       { type: "longSum", name: "count", fieldName: "count" }
+    ],
+    postAggregations: [
+      {
+        type: "expression",
+        name: "average_processing_time",
+        expression: "case_searched((count > 0),(processing_time/count),0)"
+      }
     ]
-  });
+  }
+});
+
+export const totalEventsQuery = (intervals: string, dataset: string) => ({
+  queryType: "timeseries",
+  dataSource: {
+    type: "table",
+    name: "system-events"
+  },
+  intervals: {
+    type: "intervals",
+    intervals: [intervals]
+  },
+  filter: {
+    type: "equals",
+    column: "ctx_dataset",
+    matchValueType: "STRING",
+    matchValue: dataset
+  },
+  granularity: {
+    type: "all"
+  },
+  aggregations: [
+    {
+      type: "longSum",
+      name: "total_events_count",
+      fieldName: "count"
+    }
+  ]
+});
+
+export const totalFailedEventsQuery = (intervals: string, dataset: string) => ({
+  queryType: "timeseries",
+  dataSource: {
+    type: "table",
+    name: "system-events"
+  },
+  intervals: {
+    type: "intervals",
+    intervals: [intervals]
+  },
+  filter: {
+    type: "equals",
+    column: "ctx_dataset",
+    matchValueType: "STRING",
+    matchValue: dataset
+  },
+  granularity: {
+    type: "all"
+  },
+  aggregations: [
+    {
+      type: "filtered",
+      aggregator: {
+        type: "longSum",
+        name: "total_failed_events",
+        fieldName: "count"
+      },
+      filter: {
+        type: "and",
+        fields: [
+          {
+            type: "equals",
+            column: "ctx_pdata_pid",
+            matchValueType: "STRING",
+            matchValue: "validator"
+          },
+          {
+            type: "equals",
+            column: "error_pdata_status",
+            matchValueType: "STRING",
+            matchValue: "failed"
+          }
+        ]
+      },
+      name: "total_failed_events"
+    }
+  ]
+});
+
+export const generateTimeseriesQuery = (intervals: string, dataset: string) => ({
+  queryType: "timeseries",
+  dataSource: "system-events",
+  intervals: intervals,
+  granularity: {
+    type: "all",
+    timeZone: "Asia/Kolkata"
+  },
+  filter: {
+    type: "and",
+    fields: [
+      { type: "selector", dimension: "ctx_module", value: "processing" },
+      { type: "selector", dimension: "ctx_dataset", value: dataset },
+      { type: "selector", dimension: "ctx_pdata_pid", value: "router" },
+      { type: "selector", dimension: "error_code", value: null }
+    ]
+  },
+  aggregations: [
+    { type: "longSum", name: "count", fieldName: "count" }
+  ]
+});
+
+export const generateTimeseriesQueryEventsPerHour = (intervals: string, dataset: string) => ({
+  queryType: "timeseries",
+  dataSource: "system-events",
+  intervals: intervals,
+  granularity: {
+    type: "all",
+    timeZone: "Asia/Kolkata"
+  },
+  filter: {
+    type: "and",
+    fields: [
+      { type: "selector", dimension: "ctx_module", value: "processing" },
+      { type: "selector", dimension: "ctx_dataset", value: dataset },
+      { type: "selector", dimension: "ctx_pdata_pid", value: "router" },
+      { type: "selector", dimension: "error_code", value: null }
+    ]
+  },
+  aggregations: [
+    { type: "longSum", name: "count", fieldName: "count" }
+  ]
+});
+
+export const dataLineageSuccessQuery = (intervals: string, dataset: string, column: string, value: string) => ({
+  queryType: "timeseries",
+  dataSource: {
+    type: "table",
+    name: "system-events"
+  },
+  intervals: {
+    type: "intervals",
+    intervals: [intervals]
+  },
+  filter: {
+    type: "and",
+    fields: [
+      {
+        type: "equals",
+        column: column,
+        matchValueType: "STRING",
+        matchValue: value
+      },
+      {
+        type: "equals",
+        column: "ctx_dataset",
+        matchValueType: "STRING",
+        matchValue: dataset
+      }
+    ]
+  },
+  granularity: {
+    type: "all"
+  },
+  aggregations: [
+    {
+      type: "longSum",
+      name: "count",
+      fieldName: "count"
+    }
+  ]
+});
+
+export const generateTransformationFailedQuery = (intervals: string, dataset: string) => ({
+  queryType: "timeseries",
+  dataSource: {
+    type: "table",
+    name: "system-events"
+  },
+  intervals: {
+    type: "intervals",
+    intervals: [intervals]
+  },
+  filter: {
+    type: "equals",
+    column: "ctx_dataset",
+    matchValueType: "STRING",
+    matchValue: dataset
+  },
+  granularity: {
+    type: "all"
+  },
+  aggregations: [
+    {
+      type: "filtered",
+      aggregator: {
+        type: "longSum",
+        name: "count",
+        fieldName: "count"
+      },
+      filter: {
+        type: "and",
+        fields: [
+          {
+            type: "equals",
+            column: "ctx_pdata_id",
+            matchValueType: "STRING",
+            matchValue: "TransformerJob"
+          },
+          {
+            type: "equals",
+            column: "error_pdata_status",
+            matchValueType: "STRING",
+            matchValue: "failed"
+          }
+        ]
+      },
+      name: "count"
+    }
+  ]
+});
+
+export const generateDedupFailedQuery = (intervals: string, dataset: string) => ({
+  queryType: "timeseries",
+  dataSource: {
+    type: "table",
+    name: "system-events"
+  },
+  intervals: {
+    type: "intervals",
+    intervals: [intervals]
+  },
+  filter: {
+    type: "equals",
+    column: "ctx_dataset",
+    matchValueType: "STRING",
+    matchValue: dataset
+  },
+  granularity: {
+    type: "all"
+  },
+  aggregations: [
+    {
+      type: "filtered",
+      aggregator: {
+        type: "longSum",
+        name: "count",
+        fieldName: "count"
+      },
+      filter: {
+        type: "and",
+        fields: [
+          {
+            type: "equals",
+            column: "ctx_pdata_pid",
+            matchValueType: "STRING",
+            matchValue: "dedup"
+          },
+          {
+            type: "equals",
+            column: "error_type",
+            matchValueType: "STRING",
+            matchValue: "DedupFailed"
+          }
+        ]
+      },
+      name: "count"
+    }
+  ]
+});
+
+export const generateDenormFailedQuery = (intervals: string, dataset: string) => ({
+  queryType: "timeseries",
+  dataSource: {
+    type: "table",
+    name: "system-events"
+  },
+  intervals: {
+    type: "intervals",
+    intervals: [intervals]
+  },
+  filter: {
+    type: "equals",
+    column: "ctx_dataset",
+    matchValueType: "STRING",
+    matchValue: dataset
+  },
+  granularity: {
+    type: "all"
+  },
+  aggregations: [
+    {
+      type: "filtered",
+      aggregator: {
+        type: "longSum",
+        name: "count",
+        fieldName: "count"
+      },
+      filter: {
+        type: "and",
+        fields: [
+          {
+            type: "equals",
+            column: "ctx_pdata_pid",
+            matchValueType: "STRING",
+            matchValue: "denorm"
+          },
+          {
+            type: "equals",
+            column: "error_type",
+            matchValueType: "STRING",
+            matchValue: "DenormDataNotFound"
+          }
+        ]
+      },
+      name: "count"
+    }
+  ]
+});
+
+export const generateConnectorQuery = (intervals: string, dataset: string) => ({
+  queryType: "topN",
+  dataSource: {
+    type: "table",
+    name: "system-events"
+  },
+  dimension: {
+    type: "default",
+    dimension: "ctx_source_connector",
+    outputName: "name",
+    outputType: "STRING"
+  },
+  metric: {
+    type: "dimension",
+    ordering: {
+      type: "lexicographic"
+    }
+  },
+  threshold: 1001,
+  intervals: {
+    type: "intervals",
+    intervals: [intervals]
+  },
+  filter: {
+    type: "equals",
+    column: "ctx_dataset",
+    matchValueType: "STRING",
+    matchValue: dataset
+  },
+  granularity: {
+    type: "all"
+  },
+  aggregations: [
+    {
+      type: "longSum",
+      name: "count",
+      fieldName: "count"
+    }
+  ]
+});
+
+export const generateTotalQueryCallsQuery = (time_period: string) => ({
+    end: dayjs().unix(),
+    query: `sum(sum_over_time(node_total_api_calls{entity="data-out"}[${time_period}]))`,
+    step: `${time_period}`,
+    start: dayjs().subtract(1, 'day').unix()
+});
+
+export const generateDatasetQueryCallsQuery = (dataset: string, time_period: string) => ({
+  end: dayjs().unix(),
+  step: `${time_period}`,
+  query: `sum(sum_over_time(node_total_api_calls{dataset_id="${dataset}",entity="data-out"}[${time_period}]))`,
+  start: dayjs().subtract(1, 'day').unix(),
+});
