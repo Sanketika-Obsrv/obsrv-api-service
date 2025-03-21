28d1adc570155c4547701ef6d84dad6f352d2198
JeraldJF <jeraldj@sanketika.in>
2025-03-21 12:54:30 +0530

    fix #OBS-I563 : path read as env for alerts config

--- api-service/src/services/ConfigSevice.ts
+++ api-service/src/services/ConfigSevice.ts
@@ -5,9 +5,11 @@ import _ from 'lodash';
 export class Config {
     private static instance: Config;
     private config: Record<string, any>;
+    private readonly ALERTS_CONFIG_FILE = 'alertsConfig.json';
 
     private constructor() {
-        const configPath = process.env.CONFIG_PATH || path.resolve(process.cwd(), 'src/configs/alertsConfig.json');
+        const configDir = process.env.alerts_config_path || path.resolve(process.cwd(), 'src/configs');
+        const configPath = path.join(configDir, this.ALERTS_CONFIG_FILE);
         const configContent = fs.readFileSync(configPath, 'utf8');
         this.config = JSON.parse(configContent);
     }
