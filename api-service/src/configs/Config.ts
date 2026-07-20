110ce5c4ba9b3a31d30adb4e7304d10f7b1841ef
JeraldJF <jeraldj@sanketika.in>
2026-07-20 15:58:02 +0530

    #fix: alias support defaulted to false

--- api-service/src/configs/Config.ts
+++ api-service/src/configs/Config.ts
@@ -37,7 +37,7 @@ export const config = {
     "prometheus": {
       "url": process.env.prometheus_url || "http://localhost:9090"
     },
-    "sql_query_alias_support": process.env.sql_query_alias_support || "true"
+    "sql_query_alias_support": process.env.sql_query_alias_support || "false"
   },
   "telemetry_service_config": {
     level: process.env.telemetry_log_level || "info",
